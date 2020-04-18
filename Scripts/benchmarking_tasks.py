416771c14f93d35b92f5a324c1e4ab34ea6bb3f9
Himanshu Chuphal <himanshu.chuphal07@gmail.com>
2020-04-19 00:14:04 +0200

    minor changes

--- Scripts/benchmarking_tasks.py
+++ Scripts/benchmarking_tasks.py
@@ -46,13 +46,12 @@ consoleHandler.setLevel(logging.INFO)
 fileHandler.setLevel(logging.INFO)
 logger.setLevel(logging.INFO)
 
-
+# RUN CONFIG for DL TESTING TOOLS
 #_DEFAULT_RUN_CONFIG = 'run_config1.json'
-#_DEEPXPLORE_RUN_CONFIG = 'run_config_deepX.json'
-_DLFUZZ_RUN_CONFIG = 'run_config_dlfuzz.json'
-#_SADL_RUN_CONFIG = 'run_config_sadl.json'
+#_DEFAULT_RUN_CONFIG = 'run_config_deepX.json'     # _DEEPXPLORE_RUN_CONFIG
+#_DEFAULT_RUN_CONFIG = 'run_config_dlfuzz.json'    # _DLFUZZ_RUN_CONFIG
+_DEFAULT_RUN_CONFIG = 'run_config_sadl.json'       # _SADL_RUN_CONFIG
 
-_DEFAULT_RUN_CONFIG = _DLFUZZ_RUN_CONFIG
 
 _TEMP_DIR = './temp/'
 _TEMP_CONFIG = './temp/run_config_tool.json'
